1f81a62fa617c51292cb6325b8e050be0427baa0
unknown <ramzii.hamouda@gmail.com>
2021-06-12 20:07:05 +0100

    Modifier champs de retour pour le Get des utilisateurs d'equipe RSE

--- Dtos/UtilisateurEquipeRSEReadDto.cs
+++ Dtos/UtilisateurEquipeRSEReadDto.cs
@@ -6,5 +6,8 @@ namespace RSEBack.Dtos{
         public string Prenom {get; set;}
         public string PhotoProfil {get; set;}
         public string Profession {get; set;}
+        public string Email {get; set;}
+        public string Description {get; set;}
+        public string Telephone {get; set;}
     }
 }
