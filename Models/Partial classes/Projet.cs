c555354b81934b659bcd7ea329d685a234097f23
unknown <ramzii.hamouda@gmail.com>
2021-06-13 12:44:41 +0100

    corriger problemes API projet

--- Models/Partial classes/Projet.cs
+++ Models/Partial classes/Projet.cs
@@ -1,12 +1,26 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RSEBack.Models{
     public partial class Projet{
+        [NotMapped] // to tell entity framework that this field is not in the database
+        private int _NombreImpact;
+        [NotMapped]
         public int NombreImpact{
             get{
-                return this.Impacts.Count;
+                if(this.Impacts != null){
+                _NombreImpact = this.Impacts.Count;
+                return _NombreImpact;
+                }
+                else{
+                    _NombreImpact = 0;
+                    return _NombreImpact;
+                }
+            }
+            set{
+                _NombreImpact = value;
             }
         }
 
